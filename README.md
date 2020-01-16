02cfcb9d05f62badfea43287c5b6e5de32dea452
bymaks_id <9mak$!m6>
2020-01-16 15:24:46 +0700

    first

--- README.md
+++ README.md
@@ -3,15 +3,11 @@
 ## Laravel Admin Panel
 
 [![Build Status](https://travis-ci.org/LaravelRUS/SleepingOwlAdmin.svg?branch=master)](https://travis-ci.org/LaravelRUS/SleepingOwlAdmin)
-[![StyleCI](https://styleci.io/repos/52141393/shield?branch=master)](https://styleci.io/repos/52141393)
 [![Laravel Support](https://img.shields.io/badge/Laravel-5.5--6.3-brightgreen.svg)]()
 [![PHP Support](https://img.shields.io/badge/PHP-7.1.3+-brightgreen.svg)]()
-
 [![Official Site](https://img.shields.io/badge/official-site-blue.svg)](https://sleepingowladmin.ru)
 [![Demo Site](https://img.shields.io/badge/demo-site-blue.svg)](https://demo.sleepingowladmin.ru/)
-[![Telegram Chat](https://img.shields.io/badge/telegram-chat-blue.svg)](https://t.me/prtcls)
 [![Latest Stable Version](https://poser.pugx.org/laravelrus/sleepingowl/v/stable)](https://packagist.org/packages/laravelrus/sleepingowl)
-[![Total Downloads](https://poser.pugx.org/laravelrus/sleepingowl/downloads)](https://packagist.org/packages/laravelrus/sleepingowl)
 [![License](https://poser.pugx.org/laravelrus/sleepingowl/license)](https://packagist.org/packages/laravelrus/sleepingowl)
 
 SleepingOwl Admin is an administrative interface builder for Laravel.
@@ -27,21 +23,12 @@ SleepingOwl Admin is an administrative interface builder for Laravel.
 
 ## Install dev branch
 
-  1. Change the version you need in `composer.json`
 
-  __Example__:
+  1. Run this command in the terminal:
   ```
-  "laravelrus/sleepingowl": "dev-development"
+  composer require mrubinitmegastarcom/autoadminlaravel
   ```
-
-  2. Run this command in the terminal:
-
-  ```
-  composer update laravelrus/sleepingowl
-  ```
-
-  3. Run command in the terminal for update assets:
-
+  2. Run command in the terminal for update assets:
   ```
   php artisan sleepingowl:update
   ```
--- src/Console/Commands/SectionMake.php
+++ src/Console/Commands/SectionMake.php
@@ -71,6 +71,8 @@ class SectionMake extends SectionGeneratorCommand
     {
         $stub = parent::replaceClass($stub, $name);
 
+        $stub = $this->generationFormModelClass($stub, $name);
+
         return str_replace('DummyModel', '\\'.trim((string) $this->argument('model'), '\\'), $stub);
     }
 
@@ -85,5 +87,156 @@ class SectionMake extends SectionGeneratorCommand
         $arguments[] = ['model', InputArgument::REQUIRED, 'The name of the model class'];
 
         return $arguments;
+    }
+
+    /**
+     * Обработка полученных данных из модели
+     * @param $stub
+     * @param $className
+     * @return string
+     */
+    protected function generationFormModelClass($stub, $className):string
+    {
+        $attrEdits = [];
+        $arguments = $this->argument();
+        $crud = [
+            'gridFields'=>null,
+            'editFields'=>null,
+            'modelTitle'=>null,
+            'menuIcon'=>null,
+        ];
+        if(!empty($arguments['model'])){
+            $classBase = $arguments['model'];
+            try {
+                $cls = new $classBase();
+            } catch (\Throwable $e) {
+                $cls = null;
+            } catch (\Exception $e) {
+                $cls = null;
+            }
+
+
+            if(!is_null($cls) ){
+                try{
+                    $property = $cls->getProperty();
+                } catch (\BadMethodCallException $e){
+                    $property = null;
+                }
+
+                if(!empty($property['crud'])) {
+                    $crud = $property['crud'];
+                    foreach ($crud as &$item){
+                        if(is_callable($item)){
+                            $item = $item($cls);
+                        }
+                    }
+                }
+            }
+        }
+        /*
+        | Меняем отображаемые колонки в гриде
+        | Настраиваем поля формы для редактирования
+        | Устанавливаем заголовок
+        | Устанавливаем иконку
+        | Обрабатываем разрешения (реализовать)
+        */
+        $stub = $this->displayFiledGrid( ($crud['gridFields']??['id'=>'int']), $stub );
+        $stub = $this->editColumsForm( ($crud['editFields']??['id'=>'int']), $stub );
+        $stub = $this->changeTitle( ($crud['modelTitle']??null), $stub);
+        $stub = $this->changeTitle(($crud['menuIcon']??null), $stub);
+
+        return $stub;
+    }
+
+    /**
+     * Собираем поля для отображения данных в гриде
+     * @param array $attrs
+     * @param $stub
+     * @return string
+     */
+    protected function displayFiledGrid(array $attrs, $stub):string
+    {
+        $attrEdits = [];
+        if($attrs){
+            $max = 0;
+            foreach ($attrs as $key=>$attr){
+                if($max<=self::MAX_GRID_COLOMNS){
+                    $attrEdits[] = "\AdminColumn::".$this->getInputByType($attr)."('".$key."', '".$key."')";
+                } else {
+                    break;
+                }
+                $max++;
+            }
+        }
+
+        return str_replace('DummyColumnsDisplay', implode(", \n\t\t\t\t", $attrEdits), $stub);
+    }
+
+    /**
+     * Собираем поля формы для редактирования/создания новых
+     * Example:
+     *      ColumnEdit    \AdminFormElement::text('name', 'Name')->required(),
+     *
+     * @param array $attrs
+     * @param $stub
+     * @return string
+     */
+    protected function editColumsForm(array $attrs, $stub):string
+    {
+
+        $attrEdits = [];
+        if($attrs){
+            foreach ($attrs as $key=>$attr){
+
+                $attrEdits[] = "\AdminFormElement::".$this->getInputByType($attr)."('".$key."', '".$key."')";
+            }
+        }
+        return str_replace('DummyColumnsEdit', implode(", \n\t\t\t", $attrEdits), $stub);
+
+    }
+
+    /**
+     * Меняем заголовки
+     * @param string|null $title
+     * @param $stub
+     * @return string
+     */
+    protected function changeTitle(string $title=null, $stub):string
+    {
+        if(!empty($title)){
+            $titleReplace = "'" . $title . "'";
+        } else {
+            $titleReplace = 'null';
+        }
+        return str_replace('DummyModelTitle', $titleReplace, $stub);
+    }
+
+
+    /**
+     * Меняем иконку
+     * @param string|null $icon
+     * @param $stub
+     * @return string
+     */
+    protected function changeIcon(string $icon=null, $stub):string
+    {
+        if(empty($icon)){
+            $titleReplace = 'fa fa-lightbulb-o';
+        }
+        return str_replace('DummyModelIcon', $icon, $stub);
+    }
+
+    /**
+     * Устанавливаем вид в зависимости от типа
+     * @param string $type
+     * @return string
+     */
+    protected function getInputByType(string $type)
+    {
+        switch ($type){
+            case 'Carbon' : return 'datetime';
+            case 'bool' : return 'checkbox';
+            default: return 'text';
+        }
     }
 }
