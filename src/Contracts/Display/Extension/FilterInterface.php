29d395cac6d86b224bffcc65e354deec5d73787f
bymaks <34max01@gmail.com>
2020-09-07 13:58:23 +0700

    Added case-insensitive search

--- src/Contracts/Display/Extension/FilterInterface.php
+++ src/Contracts/Display/Extension/FilterInterface.php
@@ -19,6 +19,14 @@ interface FilterInterface extends Initializable
     const NOT_CONTAINS = 'not_contains';
     const ENDS_WITH = 'ends_with';
     const NOT_ENDS_WITH = 'not_ends_with';
+
+    const BEGINS_WITH_CASE = 'begins_with_case';
+    const NOT_BEGINS_WITH_CASE = 'not_begins_with_case';
+    const CONTAINS_CASE = 'contains_case';
+    const NOT_CONTAINS_CASE = 'not_contains_case';
+    const ENDS_WITH_CASE = 'ends_with_case';
+    const NOT_ENDS_WITH_CASE = 'not_ends_with_case';
+
     const IS_EMPTY = 'is_empty';
     const IS_NOT_EMPTY = 'is_not_empty';
     const IS_NULL = 'is_null';
